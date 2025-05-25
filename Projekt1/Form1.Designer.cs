1e4e83c20e804b21051b9bf7c9f547ef4b311a0f
kapi2 <kapi2@JASKOL9>
2025-05-25 23:25:50 +0200

    Dodaj pliki projektów.

---
+++ Projekt1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Projekt1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.cmbTo = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wprowadź wartość:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(267, 110);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(174, 31);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Items.AddRange(new object[] {
+            "metr",
+            "kilometr",
+            "centymetr",
+            "milimetr"});
+            this.cmbFrom.Location = new System.Drawing.Point(267, 181);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(174, 33);
+            this.cmbFrom.TabIndex = 2;
+            // 
+            // cmbTo
+            // 
+            this.cmbTo.FormattingEnabled = true;
+            this.cmbTo.Items.AddRange(new object[] {
+            "metr",
+            "kilometr",
+            "centymetr",
+            "milimetr"});
+            this.cmbTo.Location = new System.Drawing.Point(267, 253);
+            this.cmbTo.Name = "cmbTo";
+            this.cmbTo.Size = new System.Drawing.Size(174, 33);
+            this.cmbTo.TabIndex = 5;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(267, 310);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(174, 100);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Przelicz";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(110, 451);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(77, 25);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Wynik:";
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Wartośc z:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Wartość do:";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(203, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Kalkulator Jednostek";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 1184);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.cmbTo);
+            this.Controls.Add(this.cmbFrom);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.ComboBox cmbTo;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Projekt1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double input = double.Parse(txtInput.Text); // pobranie wartości od użytkownika
+                string fromUnit = cmbFrom.SelectedItem.ToString();
+                string toUnit = cmbTo.SelectedItem.ToString();
+
+                double result = ConvertUnits(input, fromUnit, toUnit);
+                lblResult.Text = "Wynik: " + result.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Błąd danych! Upewnij się, że wpisałeś liczbę i wybrałeś jednostki.");
+            }
+        }
+
+        private double ConvertUnits(double value, string fromUnit, string toUnit)
+        {
+            // Konwersja do metrów
+            double valueInMeters = value;
+            switch (fromUnit)
+            {
+                case "metr":
+                    valueInMeters = value;
+                    break;
+                case "kilometr":
+                    valueInMeters = value * 1000;
+                    break;
+                case "centymetr":
+                    valueInMeters = value / 100;
+                    break;
+                case "milimetr":
+                    valueInMeters = value / 1000;
+                    break;
+            }
+
+            // Konwersja z metrów do docelowej jednostki
+            switch (toUnit)
+            {
+                case "metr":
+                    return valueInMeters;
+                case "kilometr":
+                    return valueInMeters / 1000;
+                case "centymetr":
+                    return valueInMeters * 100;
+                case "milimetr":
+                    return valueInMeters * 1000;
+            }
+
+            return 0; // domyślna wartość, jeśli coś pójdzie nie tak
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Dodaj jednostki do ComboBox
+            cmbFrom.Items.AddRange(new string[] { "metr", "kilometr", "centymetr", "milimetr" });
+            cmbTo.Items.AddRange(new string[] { "metr", "kilometr", "centymetr", "milimetr" });
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
